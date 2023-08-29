aabf0215785a5c1dcf36c13971c0ed610a70b239
AryaK-31 <aryakaslikar31@gmail.com>
2023-08-30 00:17:20 +0530

    buttons on cards and announcments updated

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -112,26 +112,35 @@ function Announcement() {
                                 />
                             </a>
                             <div className="p-5">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                        {announcement.title}
-                                    </h5>
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    {announcement.title}
+                                </h5>
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                     {announcement.description}
                                 </p>
-                                <button className='flex items-center justify-start gap-2' disabled={!announcement.status}>
-                                        <a  
-                                            
-                                            href={announcement.status ? announcement.link : ''}
-                                            target={announcement.status? '_blank' : ''}
+                                <button
+                                    className='flex items-center justify-start gap-2'
+                                    disabled={!announcement.status}
+                                >
+                                    {announcement.status ? (
+                                        <a
+                                            href={announcement.link}
+                                            target='_blank'
+                                            rel='noopener noreferrer'
                                             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-600 rounded-lg hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                         >
-                                            {announcement.status? 'Register' : 'closed'}
+                                            Register
                                             <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                             </svg>
                                         </a>
-                                        <p className='text-pink-600 text-lg font-medium px-2 py-1 rounded-xl'> - {announcement.date} </p>
+                                    ) : (
+                                        <button className='inline-flex items-center cursor-not-allowed bg-black text-white text-lg font-medium px-2 py-1 rounded-xl'>
+                                            Closed
+                                        </button>
+                                    )}
                                 </button>
+
                             </div>
                         </div>
                     ))}
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,33 @@ import React from 'react'
 
 export default function Card(props) {
     return (
-    <div className="max-w-sm bg-white shadow flex flex-col">
-    <div className='h-[55%] w-[100%] bg-black bg-cover bg-center' style={{ backgroundImage: `url(${props.cardImg})` }}>
+        <div className="max-w-sm bg-white shadow flex flex-col">
+            <div className='h-[55%] w-[100%] bg-black bg-cover bg-center' style={{ backgroundImage: `url(${props.cardImg})` }}>
 
-    </div>
-    <div className="pl-5 pr-1 h-[45%] w-full flex flex-col justify-center items-start bg-slate-900">
-        <a href="/">
-            <h5 className="mb-2 lg:text-lg 2xl:text-2xl font-bold tracking-tight text-pink-600">{props.sportType} - <span className="text-white lg:text-sm 2xl:text-lg font-thin">{props.date}</span></h5>
-        </a>
-        <p className="mb-3 lg:text-sm 2xl:text-lg 2xl:font-medium text-white">{props.func}</p>
-        <a target='_blank' href={props.url} className={`inline-flex items-center lg:px-[5px] lg:py-[3px] 2xl:px-3 2xl:py-2  text-xs 2xl:text-sm font-medium text-center text-white ${props.status? 'bg-slate-600' : 'bg-violet-600 hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-blue-300'}  `}>
-           <button disabled={props.status}>{!props.status ? 'Register' : 'Closed'}</button>
-        </a>
-    </div>
-</div>
-        
+            </div>
+            <div className="pl-5 pr-1 h-[45%] w-full flex flex-col justify-center items-start bg-slate-900">
+                <a href="/">
+                    <h5 className="mb-2 lg:text-lg 2xl:text-2xl font-bold tracking-tight text-pink-600">{props.sportType} - <span className="text-white lg:text-sm 2xl:text-lg font-thin">{props.date}</span></h5>
+                </a>
+                <p className="mb-3 lg:text-sm 2xl:text-lg 2xl:font-medium text-white">{props.func}</p>
+                <button
+                    disabled={props.status}
+                    className={`inline-flex items-center lg:px-[5px] lg:py-[3px] 2xl:px-3 2xl:py-2 text-xs 2xl:text-sm font-medium text-center text-white ${props.status
+                            ? 'bg-slate-600 cursor-not-allowed'
+                            : 'bg-violet-600 hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-blue-300'
+                        }`}
+                >
+                    {!props.status ? (
+                        <a target='_blank' href={props.url}>
+                            Register
+                        </a>
+                    ) : (
+                        'Closed'
+                    )}
+                </button>
+
+            </div>
+        </div>
+
     )
 }
